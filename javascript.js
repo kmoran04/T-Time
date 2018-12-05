8e87240d6f65f6d3c7345aacbbda7ce181cd21dd
kmoran04 <42983703+kmoran04@users.noreply.github.com>
2018-12-05 15:18:08 -0500

    added green line + some vehicles

--- javascript.js
+++ javascript.js
@@ -2,12 +2,6 @@ var myLat = 0;
 var myLng = 0;
 var me = new google.maps.LatLng(myLat, myLng);
 
-var path_coords_a = new Array(17);
-var path_coords_b = new Array(6);
-
-
-var stations = {"stops":[{"name":"Alewife","lat":"42.395428","long":"-71.142483","id":"place-alfcl"},{"name":"Davis","lat":"42.39674","long":"-71.121815","id":"place-davis"},{"name":"Porter Square","lat":"42.3884","long":"-71.11914899999999","id":"place-portr"},{"name":"Harvard Square","lat":"42.373362","long":"-71.118956","id":"place-harsq"},{"name":"Central Square","lat":"42.365486","long":"-71.103802","id":"place-cntsq"},{"name":"Kendall/MIT","lat":"42.36249079","long":"-71.08617653","id":"place-knncl"},{"name":"Charles/MGH","lat":"42.361166","long":"-71.070628","id":"place-chmnl"},{"name":"Park Street","lat":"42.35639457","long":"-71.0624242","id":"place-pktrm"},{"name":"Downtown Crossing","lat":"42.355518","long":"-71.060225","id":"place-dwnxg"},{"name":"South Station","lat":"42.352271","long":"-71.05524200000001","id":"place-sstat"},{"name":"Broadway","lat":"42.342622","long":"-71.056967","id":"place-brdwy"},{"name":"Andrew","lat":"42.330154","long":"-71.057655","id":"place-andrw"},{"name":"JFK/UMass","lat":"42.320685","long":"-71.052391","id":"place-jfk"},{"name":"Savin Hill","lat":"42.31129","long":"-71.053331","id":"place-shmnl"},{"name":"Fields Corner","lat":"42.300093","long":"-71.061667","id":"place-fldcr"},{"name":"Shawmut","lat":"42.29312583","long":"-71.06573796000001","id":"place-smmnl"},{"name":"Ashmont","lat":"42.284652","long":"-71.06448899999999","id":"place-asmnl"},{"name":"North Quincy","lat":"42.275275","long":"-71.029583","id":"place-nqncy"},{"name":"Wollaston","lat":"42.2665139","long":"-71.0203369","id":"place-wlsta"},{"name":"Quincy Center","lat":"42.251809","long":"-71.005409","id":"place-qnctr"},{"name":"Quincy Adams","lat":"42.233391","long":"-71.007153","id":"place-qamnl"},{"name":"Braintree","lat":"42.2078543","long":"-71.0011385","id":"place-brntn"}]};
-
 
 var myOptions = {
     zoom: 13, // The larger the zoom number, the bigger the zoom
@@ -25,11 +19,12 @@ var marker_ray;
 var stations;
 
 var station_icon = "MBTA_icon.png";
+var train_icon = "train.png";
 
 
 function init() {
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-    south = new google.maps.LatLng(stations.stops[9].lat, stations.stops[9].long);
+    south = new google.maps.LatLng(42.3519, -71.0551);
     map.panTo(south);
     renderMap();
 
@@ -76,6 +71,8 @@ function renderMap() {
     orange_line();
     blue();
     red();
+    green();
+    vehicle();
 }
 
 
@@ -295,3 +292,99 @@ function red() {
 
 }
 
+function green(){
+
+    green_helper("B");
+    green_helper("C");
+    green_helper("D");
+    green_helper("E");
+
+}
+
+function green_helper(line)
+{
+    
+    var request = new XMLHttpRequest();
+    address = "https://api-v3.mbta.com/shapes?include=stops&filter[route]=Green-" + line;
+   
+
+    request.open("GET", address, true);
+
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) 
+        {
+            theData = request.responseText;
+            paths = JSON.parse(theData);
+
+            for (i = 0; i < paths.data.length; i++) {
+                var line = paths.data[i].attributes.polyline;
+                //console.log(line);
+                var decoded = google.maps.geometry.encoding.decodePath(line);
+                var green_path = new google.maps.Polyline( {
+                    strokeColor: "#458B00",
+                    path: decoded
+
+                });
+                green_path.setMap(map);
+            }
+
+            green_ray = new Array(paths.included.length);
+
+            for (j = 0; j < paths.included.length; j++){
+                var stop = new google.maps.LatLng(paths.included[j].attributes.latitude, paths.included[j].attributes.longitude);
+                    green_ray[j] = new google.maps.Marker({
+                    position: stop,
+                    icon: station_icon,
+                    title: "<h1>" + paths.included[j].attributes.name + "</h1>"
+                });
+            
+                green_ray[j].setMap(map);
+            }
+                
+            
+        }
+    }
+
+    request.send();
+}
+
+
+function vehicle(){
+
+    var request = new XMLHttpRequest();
+    address = "https://api-v3.mbta.com/vehicles?include=stop&filter[route]=Red";
+
+    request.open("GET", address, true);
+
+    request.onreadystatechange = function() {
+
+        if (request.readyState == 4 && request.status == 200) 
+        {
+            theData = request.responseText;
+            trains = JSON.parse(theData);
+         
+            train_ray = new Array(trains.data.length);
+           
+            for (i = 0; i < trains.data.length; i++) {
+                lat = trains.data[i].attributes.latitude;
+                lon = trains.data[i].attributes.longitude;
+                var loc = new google.maps.LatLng(lat, lon);
+                station = trains.included[i].attributes.name;
+                console.log(station);
+                console.log(i);
+
+                train_ray[i] = new google.maps.Marker({
+                    position: loc,
+                    icon: train_icon,
+                    title: "<h1>" + station + "</h1>"
+                });
+
+                train_ray[i].setMap(map);
+            }
+            
+        }
+    }
+
+    request.send();
+
+}
